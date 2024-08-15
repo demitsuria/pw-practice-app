9e7cc310d0be234565a33000851f55ca8031f79b
demitsuria <demitsuria@gmail.com>
2024-08-15 09:57:28 +0200

    14.08

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,32 @@
+import { test } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('Locator syntax rules', async ({ page }) => {
+
+    // by Tag name
+    await page.locator('input').first().click()
+
+    //by ID
+    page.locator('#inputEmail1')
+
+    // by Class value
+    page.locator('.shape-rectangle')
+
+    //by attribute
+    page.locator('[placeholder="Email"]')
+
+    // by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input [placeholder="Email"] [nbinput]')
+
+    // by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]')
+})

6b62c64618143fc003bd0a142928bc7699824933
demitsuria <demitsuria@gmail.com>
2024-08-15 18:03:25 +0200

    15.08.24

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
 
@@ -29,4 +29,93 @@ test('Locator syntax rules', async ({ page }) => {
 
     // by XPath (NOT RECOMMENDED)
     page.locator('//*[@id="inputEmail1"]')
+})
+
+test('User facing locators', async ({ page }) => {
+    await page.getByRole('textbox', { name: "Email" }).first().click()
+    await page.getByRole('button', { name: "Sign In" }).first().click()
+
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe').first().click()
+
+    await page.getByText('Using The Grid').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+
+    await page.getByTestId('SignIn')
+
+
+
+})
+
+test('locating child elements', async ({ page }) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page
+        .locator('nb-card')
+        .getByRole('button', { name: "Sign in" })
+        .first()
+        .click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
+
+})
+
+test('locating parent elements', async ({ page }) => {
+    await page.locator('nb-card', { hasText: "Using the Grid" }).getByRole('textbox', { name: "Email" }).click()
+    await page.locator('nb-card', { has: page.locator('#inputEmail1') }).getByRole('textbox', { name: "Email" }).click()
+
+    await page.locator('nb-card').filter({ hasText: "Basic Form" }).getByRole('textbox', { name: "Email" }).click()
+    await page.locator('nb-card', { has: page.locator('.status-danger') }).getByRole('textbox', { name: "Password" }).click()
+
+    await page.locator('nb-card').filter({ has: page.locator('nb-checkbox') }).filter({ hasText: ("Sign In") }).getByRole('textbox', { name: "Email" }).click()
+
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', { name: "Email" }).click()
+
+})
+
+test('Reusing the locators', async ({ page }) => {
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic Form" })
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+
+    await emailField.fill('test@test.com')
+    await basicForm.getByRole('textbox', { name: "Password" }).fill('loltrol')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button').click()
+
+    await expect(emailField).toHaveValue('test@test.com')
+})
+
+test('Extracting values', async ({ page }) => {
+    //single test value
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic Form" })
+    const buttonText = await basicForm.locator('button').textContent()
+    expect(buttonText).toEqual('Submit')
+
+    //all text values
+
+    const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonsLabels).toContain('Option 1')
+
+    //Value of the input field
+
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    const placeholderValue = await emailField.getAttribute('placeholder')
+    expect(placeholderValue).toEqual('Email')
+})
+
+
+test('assertions', async ({ page }) => {
+    const basicFormButton = page.locator('nb-card').filter({ hasText: "Basic Form" }).locator('button')
+    //General assertions
+
+    const value = 5
+    expect(value).toEqual(5)
+
 })
